71e2575af1eb6b584002fa37bfa4122f154203f5
Junior Rodrigues <juniorpsicorodrigues@gmail.com>
2022-09-27 12:05:28 -0300

    feat: final project

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
-const arrayMap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
-function callback(elemento, index, array){
-  return elemento +10  
+const arrayMap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const callback = ((element) => {
+   return element + 4 
+})
+function methodMap(array, callback) {
+    const newMap = []
+    for (let i = 0; i < array.length; i++) {
+        newMap.push(callback (array[i], i, array))
+    }
+    return newMap
 }
-function map(array, callback){
-  let novoArrayMap = []
-  for (let i = 0; i <array.length; i++){
-    novoArrayMap.push(callback (array[i], i, array))
-  }
-  return novoArrayMap
+methodMap(arrayMap, callback)
+function methodFilter(arrayMap) {
+    const filterNumber = []
+    for (let i = 0; i < arrayMap.length; i++) {
+        if (arrayMap[i] === 6 ) {
+            filterNumber.push(arrayMap[i])
+        }
+    }
+    return filterNumber
 }
-map(arrayMap, callback)
-function FiltrarImpar(arrayMap){
-  let arrayImpar = []
-  for (let i = 0; i < arrayMap.length; i++) {
-    if (arrayMap[i] % 2 == 1) {
-      arrayImpar.push(arrayMap[i])
+methodFilter(arrayMap)
+const callbackReduce = ((elementInicial, elementValue) => {
+    return elementInicial + elementValue
+})
+function methodReduce(array, callback, initialValue = 0) {
+    for (let i = 0; i < arrayMap.length; i++) {
+        initialValue = callback(initialValue, array[i])
+    }
+    return initialValue
+}
+methodReduce(arrayMap, callbackReduce)
+function methodFind(arrayMap) {
+    const findNumber = []
+    for( let i = 0; i < arrayMap.length; i++) {
+        if (arrayMap[i] > 8) {
+            return arrayMap[i]
+        }
     } 
-  } 
-    return arrayImpar
+    return findNumber
 }
-FiltrarImpar(arrayMap)
-function callbackReduce(acumulador, valor) {
-  return acumulador + valor
+methodFind(arrayMap)
+function methodIncludes(arrayMap) {
+    for (let i = 0; i < arrayMap.length; i++) {
+        if (arrayMap[i] === 10){
+            return true
+        }
+        else {
+            return false
+        }
+    }
 }
-function reduce(array, callback, initialValue = 0) {
-  for( let i = 0; i < array.length; i++){
-   initialValue = callback(initialValue, array[i])
-  }
-  return initialValue
+methodIncludes(arrayMap)
+function callbackIndex() {
+    const callbackIndexOf = "Oi, sou um jovem aprendiz nesse mundo da programação!"
+    return callbackIndexOf
 }
-reduce(arrayMap,callbackReduce)
-
+callbackIndex()
+function methodIndexOf(callbackIndexOf) {
+    const resultIndexOf = ""
+    for (let i = 0; i < callbackIndexOf; i++){
+        if (callbackIndexOf[i] === "j") {
+            return resultIndexOf.push(callbackIndex(callbackIndexOf[i]))
+        }
+    }
+    return resultIndexOf
+}
+methodIndexOf()
 
